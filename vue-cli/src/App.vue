1b189c240688a08f4d00fab14437da7654f25986
rohan <rockybelboa09@gmail.com>
2019-06-05 02:06:48 +0530

    vue custom directives

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,167 +1,32 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <h1>File a Complaint</h1>
-                    <hr>
-                    <div class="form-group">
-                        <label for="email">Mail</label>
-                        <input
-                                type="text"
-                                id="email"
-                                class="form-control"
-                                v-model.lazy="userData.email"
-                                >
-                    </div>
-                    @{{ userData.email }}
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input
-                                type="password"
-                                id="password"
-                                class="form-control"
-                                v-model="userData.password"
-                                >
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age</label>
-                        <input
-                                type="number"
-                                id="age"
-                                class="form-control"
-                                v-model="userData.age"
-                                >
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="message">Message</label><br>
-                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                            id="message"
-                            rows="5"
-                            class="form-control"
-                            v-model="userData.message"
-                            ></textarea>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <div class="form-group">
-                        <label for="sendmail">
-                            <input
-                                    type="checkbox"
-                                    id="sendmail"
-                                    value="SendMail"
-                                    v-model="sendmailArr"
-                                    > Send Mail
-                        </label>
-                        <label for="sendInfomail">
-                            <input
-                                    type="checkbox"
-                                    id="sendInfomail"
-                                    value="SendInfoMail"
-                                    v-model="sendmailArr"
-                                    > Send Infomail
-                        </label>
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
-                        <input
-                                type="radio"
-                                id="male"
-                                value="Male"
-                                v-model="gender"
-                                > Male
-                    </label>
-                    <label for="female">
-                        <input
-                                type="radio"
-                                id="female"
-                                value="Female"
-                                v-model="gender"
-                                > Female
-                    </label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-                    <label for="priority">Priority</label>
-                    <select
-                            id="priority"
-                            class="form-control"
-                            v-model="selectedVal">
-                        <option v-for="priority in priorities" 
-                        :key="priority" 
-                        :selected="priority == 'Medium'"
-                        >{{ priority }}</option>
-                    </select>
-                </div>
-            </div>
-            <hr>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button
-                            class="btn btn-primary" @click.prevent="submited">Submit!
-                    </button>
-                </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row" v-if="isSubmit">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Mail: {{ userData.email }}</p>
-                        <p>Password:{{ userData.password }}</p>
-                        <p>Age:{{ userData.age }}</p>
-                        <p style="white-space:pre">Message:{{ userData.message }} </p>
-                        <p><strong>Send Mail?</strong></p>
-                        <ul>
-                            <li v-for="items in sendmailArr" :key="items">{{ items }}</li>
-                        </ul>
-                        <p>Gender:{{ gender }}</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+           <h1>Built in Directive</h1>
+           <p v-highlight:background.delayed="'blue'">ss</p>
+           <p v-local-highlight:background.delayed="'blue'">ss</p>
+       </div> 
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-                userData:{
-                    email:'',
-                    password:'',
-                    age:'',
-                    message:'this is a message',
-                },
-                sendmailArr:[],
-                gender:'Male',
-                selectedVal:'Low',
-                priorities:['High','Medium','Low'],
-                isSubmit: false
-            }
-        },
-        methods:{
-            submited() {
-                this.isSubmit = true
+      directives:{
+        'local-highlight':{
+            bind(el, binding, vnode) {
+                var delay = 0
+                if (binding.modifiers['delayed']) {
+                    delay = 3000
+                }
+                setTimeout(() => {
+                    if (binding.args == 'background') {
+                        el.style.backgroundColor = binding.value
+                    } else {
+                        el.style.backgroundColor = binding.value
+                    }
+                }, delay);
             }
         }
+      }
     }
 </script>
 
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight',{
+    bind(el, binding, vnode) {
+      var delay = 0
+      if (binding.modifiers['delayed']) {
+        delay = 3000
+      }
+      setTimeout(() => {
+          if (binding.args == 'background') {
+            el.style.backgroundColor = binding.value
+          } else {
+            el.style.backgroundColor = binding.value
+          }
+      }, delay);
+    }
+})
+
 export const eventBus = new Vue({
     methods: {
         changeAge(age) {
