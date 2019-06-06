97c5624abf821c4ac8169ae72d0d8807ef4c0094
rohan <rockybelboa09@gmail.com>
2019-06-07 02:28:44 +0530

    named routes commit

--- vue-cli/src/components/Header.vue
+++ vue-cli/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <ul class="nav nav-pills">
-        <router-link tag="li" to="/home" active-class="active"><a>Home</a></router-link>
-        <router-link tag="li" to="/user" active-class="active"><a>User</a></router-link>
-        <router-link tag="li" to="/" active-class="active" exact><a>Logout</a></router-link>
+        <router-link tag="li" :to="{name:'home'}" active-class="active"><a>Home</a></router-link>
+        <router-link tag="li" :to="{name:'users'}" active-class="active"><a>User</a></router-link>
+        <router-link tag="li" :to="{name:'login'}" active-class="active" exact><a>Logout</a></router-link>
     </ul>
 </template>
 
